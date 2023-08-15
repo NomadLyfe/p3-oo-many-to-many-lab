68d419a9ba51facd3196f48adbe5b2fe7f1ca1dd
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-08-15 18:45:26 -0400

    complete lab

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,54 @@
 class Author:
-    pass
+    all = []
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
 
+    def contracts(self):
+        return [c for c in Contract.all if c.author == self]
+
+    def books(self):
+        return [c.book for c in Contract.all if c.author == self]
+    
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+    
+    def total_royalties(self):
+        return sum([c.royalties for c in Contract.all if c.author == self])
 
 class Book:
-    pass
-
+    all = []
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+    
+    def contracts(self):
+        return [c for c in Contract.all if c.book == self]
+    
+    def authors(self):
+        return [c.author for c in Contract.all if c.book == self]
 
 class Contract:
-    pass
+    all = []
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise Exception
+        else:
+            self.author = author
+        if not isinstance(book, Book):
+            raise Exception
+        else:
+            self.book = book
+        if type(date) != type(' '):
+            raise Exception
+        else:
+            self. date = date
+        if type(royalties) != type(1):
+            raise Exception
+        else:
+            self. royalties = royalties
+        Contract.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls):
+        return sorted(cls.all, key = lambda c: c.date)
